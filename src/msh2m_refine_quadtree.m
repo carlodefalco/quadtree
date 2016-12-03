ae59b247ee6fb49ed14511e3673998a656acd3b7
Carlo de Falco <carlo.defalco@polimi.it>
2016-12-03 23:38:13 +0100

    fix tests and demos

--- src/msh2m_refine_quadtree.m
+++ src/msh2m_refine_quadtree.m
@@ -110,10 +110,29 @@ endfunction
 function res = oncorner (ii)
   res = ismember (ii, 1:4);
 endfunction
+
 %!test
 %! x = y = 1:2;
 %! msh = msh2m_refine_quadtree ...
 %!   (msh2m_quadtree (x, y, 1, 1:4), 1);
 %! assert (size (msh.p), [2 9]);
-%! msh = msh2m_refine_quadtree (msh, 1);
-%! assert (size (msh.p), [2 14]);
+
+%!demo
+%! # Mesh definition.
+%! x = linspace(0, 5, 10);
+%! y = linspace(0, 3,  5);
+%! 
+%! region = 1;
+%! sides = 1:4;
+%! 
+%! msh = msh2m_quadtree(x, y, region, sides);
+%! 
+%! # Mesh refinement.
+%! msh = msh2m_refine_quadtree(msh, [4, 8]);
+%! msh = msh2m_refine_quadtree(msh, [40 48 52]);
+%! msh = msh2m_refine_quadtree(msh, [44 12 43 59]);
+%! msh = msh2m_refine_quadtree(msh, [33 74 78]);
+%! 
+%! # Show refined mesh.
+%! figure; quadmesh(msh, "show_cell_numbers", "show_node_numbers");
+
