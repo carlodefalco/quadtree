2fb34e52c603e48bc4b5b1ade182ba20d616f849
elauksap <pasquale.africa@libero.it>
2016-12-13 17:19:37 +0100

    Fix reduced indexing

--- src/bim2a_quadtree_solve.m
+++ src/bim2a_quadtree_solve.m
@@ -1,10 +1,12 @@
 function u = bim2a_quadtree_solve(msh, A, f, u, dnodes)
     # Solve for non-hanging and non-Dirichlet nodes.
-    non_hanging = find(msh.full_to_reduced);
-    intnodes = msh.full_to_reduced(setdiff(non_hanging, dnodes));
+    non_hanging = msh.full_to_reduced(msh.full_to_reduced != 0);
+    reduced_dnodes = msh.full_to_reduced(dnodes);
+    
+    intnodes = setdiff(non_hanging, reduced_dnodes);
     
     u(intnodes) = A(intnodes, intnodes) \ ...
-                  (f(intnodes) - A(intnodes, dnodes) * u(dnodes));
+                  (f(intnodes) - A(intnodes, reduced_dnodes) * u(reduced_dnodes));
     
     # Interpolate solution at the hanging nodes.
     hanging = find(!msh.full_to_reduced);
