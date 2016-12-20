81170948f31f70c05f78f846a023f05d07544f10
elauksap <pasquale.africa@libero.it>
2016-12-20 12:14:45 +0100

    Parallel refinement check

---
+++ src/msh2m_to_refine.m
@@ -0,0 +1,41 @@
+function to_refine = msh2m_to_refine(msh, alpha, psi, f, g, u, iel, tol)
+    nodes = msh.t(1:4, iel);
+
+    x_iel = [min(msh.p(1, nodes)), max(msh.p(1, nodes))];
+    y_iel = [min(msh.p(2, nodes)), max(msh.p(2, nodes))];
+
+    hx = diff(x_iel);
+    hy = diff(y_iel);
+    
+    msh_iel = msh2m_quadtree(x_iel, y_iel);
+    msh_iel = msh2m_refine_quadtree(msh_iel, 1);
+
+    # Build local matrix.
+    x_iel = msh_iel.p(1, :).';
+    y_iel = msh_iel.p(2, :).';
+    
+    # Assemble system.
+    A_iel = bim2a_quadtree_advection_diffusion(msh_iel, alpha(msh_iel), psi(x_iel, y_iel));
+
+    dnodes_iel = msh2m_nodes_on_sides(msh_iel, 1:4);
+
+    rhs_iel = bim2a_quadtree_rhs(msh_iel, f(msh_iel), g(x_iel, y_iel));
+
+    # Compute boundary conditions.
+    # Original vertices.
+    u_dnodes = zeros(2 * numel(nodes) + 1, 1);
+    u_dnodes(msh_iel.t(1:4, 1)) = u(nodes);
+
+    # Added vertices (side midpoints).
+    hanging_sides = [1, 3, 4, 2;
+                     3, 4, 2, 1];
+    u_dnodes((numel(nodes)+1) : end - 1) = mean(u_dnodes(hanging_sides));
+    
+    # Vertex in the middle of the element.
+    u_dnodes(end) = 0;
+
+    # Compute solution and evaluate error.
+    u_iel = bim2a_quadtree_solve(msh_iel, A_iel, rhs_iel, u_dnodes, dnodes_iel);
+    
+    to_refine = (abs(u_iel(end) - mean(u(nodes))) > tol * sqrt(hx * hy));
+endfunction
--- src/test_adaptivity.m
+++ src/test_adaptivity.m
@@ -25,7 +25,6 @@ for i = 1:10
 
     # Define parameters and exact solution.
     epsilon = 1;
-    lambda = 1 / epsilon;
 
     u_ex = sin(x) .* cos(2*y);
 
@@ -53,53 +52,15 @@ for i = 1:10
     fpl_vtk_write_field_quadmesh(filename, msh, {u, "u"; u_ex, "u_ex"}, {}, 1);
 
     # Determine elements to be refined.
-    tol = 1e-3;
-    to_refine = false(1, Nelements);
-    
+    tol = 1e-2;
     refineable_elements = find(!any(msh.children));
     
-    for iel = refineable_elements
-        nodes = msh.t(1:4, iel);
-
-        x_iel = [min(msh.p(1, nodes)), max(msh.p(1, nodes))];
-        y_iel = [min(msh.p(2, nodes)), max(msh.p(2, nodes))];
-
-        msh_iel = msh2m_quadtree(x_iel, y_iel);
-        msh_iel = msh2m_refine_quadtree(msh_iel, 1);
-
-        # Build local matrix.
-        x_iel = msh_iel.p(1, :).';
-        y_iel = msh_iel.p(2, :).';
-        
-        # Assemble system.
-        A_iel = bim2a_quadtree_advection_diffusion(msh_iel, alpha(msh_iel), psi(x_iel, y_iel));
-
-        dnodes_iel = msh2m_nodes_on_sides(msh_iel, 1:4);
-
-        rhs_iel = bim2a_quadtree_rhs(msh_iel, f(msh_iel), g(x_iel, y_iel));
-
-        # Compute boundary conditions.
-        # Original vertices.
-        u_dnodes = zeros(2 * numel(nodes) + 1, 1);
-        u_dnodes(msh_iel.t(1:4, 1)) = u(nodes);
-
-        # Added vertices (side midpoints).
-        hanging_sides = [1, 3, 4, 2;
-                         3, 4, 2, 1];
-        u_dnodes((numel(nodes)+1) : end - 1) = mean(u_dnodes(hanging_sides));
-        
-        # Vertex in the middle of the element.
-        u_dnodes(end) = 0;
-
-        # Compute solution and evaluate error.
-        u_iel = bim2a_quadtree_solve(msh_iel, A_iel, rhs_iel, u_dnodes, dnodes_iel);
-        
-        if (abs(u_iel(end) - mean(u(nodes))) > tol)
-            to_refine(iel) = true;
-        endif
-    endfor
+    to_refine = false(1, Nelements);
+    to_refine(refineable_elements) = ...
+        parcellfun(4, @(iel) msh2m_to_refine(msh, alpha, psi, f, g, u, iel, tol),
+                   num2cell(refineable_elements));
     
-    fprintf("Elements to refine = %d\n\n", sum(to_refine));
+    fprintf("Elements to refine = %d / %d\n\n", sum(to_refine), numel(refineable_elements));
     
     if (!any(to_refine))
         break;
