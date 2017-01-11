ca5c7d6c52cded2f5abf6231ef3c166da66d6713
elauksap <pasquale.africa@libero.it>
2017-01-11 18:23:07 +0100

    Mesh properties function

---
+++ src/bim2c_quadtree_mesh_properties.m
@@ -0,0 +1,11 @@
+function [msh] = bim2c_quatree_mesh_properties(msh)
+  ## Check input.
+  if nargin != 1
+    error("bim2c_quatree_mesh_properties: wrong number of input parameters.");
+  elseif !(isstruct(msh) && isfield(msh, "p") && isfield (msh, "t") && isfield(msh, "e"))
+    error("bim2c_quatree_mesh_properties: first input is not a valid mesh structure.");
+  endif
+
+  [msh.wjacdet, msh.area, msh.shg] = ...
+      msh2m_quatree_geometrical_properties(msh, "wjacdet", "area", "shg");
+endfunction
