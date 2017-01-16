5831562eee49d319a3f0346403ca1d89b13f68de
elauksap <pasquale.africa@libero.it>
2017-01-16 14:18:51 +0100

    Improve recursive refinement

--- src/msh2m_quadtree_refine_recursive.m
+++ src/msh2m_quadtree_refine_recursive.m
@@ -1,10 +1,10 @@
-function msh = msh2m_quadtree_refine_recursive(msh, refinelist)
+function msh = msh2m_quadtree_refine_recursive(msh, refinelist, to_refine = [])
     for ii = 1 : numel(refinelist)
-        msh = do_refinement_recursive(msh, refinelist(ii));
+        msh = do_refinement_recursive(msh, refinelist(ii), to_refine);
     endfor
 endfunction
 
-function msh = do_refinement_recursive(msh, iel)
+function msh = do_refinement_recursive(msh, iel, to_refine)
     nodes = msh.t(1:4, iel);
     hanging_nodes = msh.hanging(:, nodes);
     
@@ -43,6 +43,11 @@ function msh = do_refinement_recursive(msh, iel)
     
     neighbors = neighbors(is_hanging_neighbor);
     
+    # Ignore neighbors that are already marked to be refined.
+    if (!isempty(to_refine))
+        neighbors = setdiff(neighbors, find(to_refine(neighbors)));
+    endif
+    
     ## Refine all neighbors.
     msh = msh2m_quadtree_refine_recursive(msh, neighbors);
     
@@ -51,20 +56,20 @@ function msh = do_refinement_recursive(msh, iel)
 endfunction
 
 %!demo
-%!  
-%!  n = 10;
-%!  
-%!  # Mesh definition.
-%!  x = linspace(0, 1, n);
-%!  y = linspace(0, 2, n);
-%!  
-%!  region = 1;
-%!  sides = 1:4;
-%!  
-%!  msh = msh2m_quadtree(x, y, region, sides);
-%!  
-%!  # Try recursive refinement.
-%!  msh = msh2m_quadtree_refine_recursive (msh, 22);
-%!  msh = msh2m_quadtree_refine_recursive (msh, [84 81 98 106]);
-%!  quadmesh(msh, "show_cell_numbers", "show_node_numbers");
-%!  
+%! # Mesh definition.
+%! 
+%! n = 10;
+%! 
+%! x = linspace(0, 1, n);
+%! y = linspace(0, 2, n);
+%! 
+%! region = 1;
+%! sides = 1:4;
+%! 
+%! msh = msh2m_quadtree(x, y, region, sides);
+%! 
+%! # Try recursive refinement.
+%! msh = msh2m_quadtree_refine_recursive (msh, 22);
+%! msh = msh2m_quadtree_refine_recursive (msh, [84 81 98 106]);
+%! quadmesh(msh, "show_cell_numbers", "show_node_numbers");
+%! 
--- src/org_mis/test_mis_2D.m
+++ src/org_mis/test_mis_2D.m
@@ -104,6 +104,6 @@ for i = 1 : 15
     if (!any(to_refine))
         break;
     else
-        msh = msh2m_quadtree_refine_recursive(msh, find(to_refine));
+        msh = msh2m_quadtree_refine_recursive(msh, find(to_refine), to_refine);
     endif
 endfor
