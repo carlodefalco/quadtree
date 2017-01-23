28f022dbff8ac95cd9ac20e8cb60854809f55754
elauksap <pasquale.africa@libero.it>
2017-01-23 16:56:39 +0100

    Add function to compute a reconstructed gradient

---
+++ src/bim2c_quadtree_pde_reconstructed_gradient.m
@@ -0,0 +1,21 @@
+function [du_x, du_y] = bim2c_quadtree_pde_reconstructed_gradient(msh, u)
+    Nnodes = columns(msh.p);
+    
+    du_x = du_y = zeros(Nnodes, 1);
+    
+    non_hanging = find(msh.full_to_reduced);
+    hanging = find(!msh.full_to_reduced);
+    
+    for ii = non_hanging
+        ## Compute adjacent sides (those sharing a node with current element).
+        [~, sides] = ind2sub(size(msh.sides), find(msh.sides == ii));
+        
+        ## Ignore hanging edge parent.
+        parent = msh.hanging(msh.hanging_sides(sides) != 0);
+        sides(parent) = [];
+    endfor
+    
+    ## Interpolate gradient at the hanging nodes.
+    du_x(hanging) = mean(du_x(msh.hanging(:, hanging)));
+    du_y(hanging) = mean(du_y(msh.hanging(:, hanging)));
+endfunction
