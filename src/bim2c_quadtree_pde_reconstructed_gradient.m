d9bfe2098f7a546667fcf34d46dde584b920ea74
elauksap <pasquale.africa@libero.it>
2017-01-31 16:15:51 +0100

    Fix reconstructed gradient formula

--- src/bim2c_quadtree_pde_reconstructed_gradient.m
+++ src/bim2c_quadtree_pde_reconstructed_gradient.m
@@ -13,9 +13,20 @@ function [du_x, du_y] = bim2c_quadtree_pde_reconstructed_gradient(msh, du)
         ## Ignore hanging sides.
         sides(find(msh.hanging_sides(sides))) = [];
         
-        ## Compute gradient.
-        du_x(ii) = mean(du(sides(msh.orien(sides))));
-        du_y(ii) = mean(du(sides(!msh.orien(sides))));
+        ## Identify horizontal and vertical sides.
+        sides_x = sides( msh.orien(sides));
+        sides_y = sides(!msh.orien(sides));
+        
+        ## Determine the length of each side.
+        sides_x_nodes = msh.sides(:, sides_x);
+        sides_y_nodes = msh.sides(:, sides_y);
+        
+        h_x = abs(diff(reshape(msh.p(1, sides_x_nodes), size(sides_x_nodes)))).';
+        h_y = abs(diff(reshape(msh.p(2, sides_y_nodes), size(sides_y_nodes)))).';
+        
+        ## Compute the reconstructed gradient.
+        du_x(ii) = sum(du(sides_x) ./ h_x) / sum(1 ./ h_x);
+        du_y(ii) = sum(du(sides_y) ./ h_y) / sum(1 ./ h_y);
     endfor
     
     ## Interpolate gradient at the hanging nodes.
