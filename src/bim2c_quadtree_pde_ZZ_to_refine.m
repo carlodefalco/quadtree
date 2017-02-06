0b2a4dfc49d94145ba4175261fefd57656da1175
elauksap <pasquale.africa@libero.it>
2017-02-06 12:11:20 +0100

    Compute area if not available

--- src/bim2c_quadtree_pde_ZZ_to_refine.m
+++ src/bim2c_quadtree_pde_ZZ_to_refine.m
@@ -7,7 +7,12 @@ function [to_refine] = bim2c_quadtree_pde_ZZ_to_refine(msh, estimator, tol, crit
         case 1
             to_refine(refineable_elements) = (estimator > tol);
         case 2
-            to_refine(refineable_elements) = (estimator ./ msh.area(:, refineable_elements) > tol);
+            if (!isfield(msh, "area"))
+                [~, msh.area] = ...
+                    msh2m_quadtree_geometrical_properties(msh, [], [], "area");
+            endif
+            
+            to_refine(refineable_elements) = (estimator ./ msh.area(refineable_elements) > tol);
         case 3
             [estimator_sorted, idx_sorted] = sort(estimator, "descend");
             estimator_sum = cumsum(estimator_sorted);
