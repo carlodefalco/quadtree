e45d0b9fb5b244a8fdfad239d63d1d3b8f167417
elauksap <pasquale.africa@libero.it>
2017-02-07 18:28:21 +0100

    Patch test

--- src/bim2a_quadtree_laplacian.m
+++ src/bim2a_quadtree_laplacian.m
@@ -1,3 +1,84 @@
 function A = bim2a_quadtree_laplacian (msh, D)
-    A = bim2a_quadtree_advection_diffusion(msh, D, zeros(columns(msh.p), 1));
+  real_elem = find (! any (msh.children));
+  
+  nprocs = 4;
+  
+  # Serial process if mesh has few elements.
+  if (columns(msh.t) < 100)
+    nprocs = 1;
+  endif
+  
+  [II, JJ, VV] = parcellfun(nprocs, @(iel) local_to_global(msh, D, iel),
+                            num2cell(real_elem), 'UniformOutput', false,
+                            'VerboseLevel', 0);
+  
+  II = vertcat(II{:});
+  JJ = vertcat(JJ{:});
+  VV = vertcat(VV{:});
+  
+  A = sparse (II, JJ, VV, numel (msh.reduced_to_full),
+              numel (msh.reduced_to_full));
+  
+  
+    ## Projection method.
+#     persistent A_loc = [1 -0.5 0 -0.5;
+#                         -0.5 1 -0.5 0;
+#                         0 -0.5 1 -0.5;
+#                         -0.5 0 -0.5 1];
+#     
+#     Nnodes = columns(msh.p);
+# 
+#     for inode = 1 : 4
+#         for jnode = 1 : 4
+#             II(inode, jnode, :) = msh.t(inode, :);
+#             JJ(inode, jnode, :) = msh.t(jnode, :);
+#             VV(inode, jnode, :) = D .* A_loc(inode, jnode);
+#         endfor
+#     endfor
+#     
+#     A = sparse(II, JJ, VV, Nnodes, Nnodes);
+#     A = msh.P.' * A * msh.P;
+    
+    ## Constraints method.
+#     hanging = find(any(msh.hanging));
+#     non_hanging = find(!any(msh.hanging));
+#     
+#     II = repmat(1:numel(hanging), [2, 1])(:);
+#     JJ = msh.full_to_reduced(msh.hanging(:, hanging))(:);
+#     VV = -0.5 * ones(size(II));
+#     
+#     Ann = A(non_hanging, non_hanging);
+#     Anh = A(non_hanging, hanging);
+#     
+#     E = sparse(II, JJ, VV, numel(hanging), numel(non_hanging));
+#     D = speye(numel(hanging));
+#     
+#     A = Ann - Anh * (D \ E);
+endfunction
+
+function [II, JJ, VV] = local_to_global (msh, D, iel)
+  coords = msh.p(:, msh.t(1:4, iel));
+  
+  hx = diff(coords(1, [1, 2]));
+  hy = diff(coords(2, [1, 3]));
+  
+  ## A_loc = local_matrix (hx, hy, D(iel));
+  A_loc = __laplacian_local_matrix__ (hx, hy, D(iel));
+  
+  [II, JJ, VV] = bim2a_quadtree_local_to_global(msh, A_loc, iel);
+endfunction
+
+function A_loc = local_matrix (hx, hy, D_loc)
+  # The diagonal elements are accounted for twice, by
+  # A_loc += A_loc';
+  # so they are divided by 2.
+  A_loc = speye(4) * (hx^2 + hy^2) / (4 * hx * hy);
+  
+  A_loc(1, 2) = -hy / (2 * hx);
+  A_loc(1, 4) = -hx / (2 * hy);
+  A_loc(2, 3) = -hx / (2 * hy);
+  A_loc(3, 4) = -hy / (2 * hx);
+  A_loc += A_loc';
+  
+  A_loc *= D_loc;
 endfunction
---
+++ src/test_sol.m
@@ -0,0 +1,105 @@
+clear all;
+close all;
+clc;
+
+addpath(canonicalize_file_name("./test_sol/"));
+
+function [P] = projector(msh)
+    Nnodes = columns(msh.p);
+    
+    hanging = find(any(msh.hanging));
+    non_hanging = find(!any(msh.hanging));
+    
+    II = non_hanging;
+    JJ = msh.full_to_reduced(non_hanging);
+    VV = ones(size(II));
+    
+    P = sparse(II, JJ, VV, Nnodes, numel(non_hanging));
+    
+    II = repmat(hanging, [2, 1])(:);
+    JJ = msh.full_to_reduced(msh.hanging(:, hanging))(:);
+    VV = 0.5 * ones(size(II));
+    
+    P += sparse(II, JJ, VV, Nnodes, numel(non_hanging));
+endfunction
+
+# Mesh definition.
+x = linspace(0, 1, 3);
+y = linspace(0, 1, 3);
+
+msh = msh2m_quadtree(x, y);
+msh = msh2m_quadtree_refine(msh, 1);
+
+msh.P = projector(msh);
+
+i = 1;
+
+# Build global matrix.
+Nnodes = columns(msh.p);
+Nelements = columns(msh.t);
+
+x = msh.p(1, :).';
+y = msh.p(2, :).';
+
+# Define parameters and exact solution.
+epsilon = 1;
+
+u_ex = x .* y;
+du_x_ex = y;
+du_y_ex = x;
+
+# Assemble system.
+alpha = epsilon * ones(Nelements, 1);
+
+A = bim2a_quadtree_laplacian(msh, alpha);
+
+dnodes = msh2m_nodes_on_sides(msh, 1:4);
+
+f = zeros(Nelements, 1);
+g = ones(Nnodes, 1);
+rhs = bim2a_quadtree_rhs(msh, f, g);
+
+# Compute solution and error.
+u = bim2a_quadtree_solve(msh, A, rhs, u_ex, dnodes);
+
+[du] = bim2c_quadtree_pde_edge_gradient(msh, u);
+[du_x, du_y] = bim2c_quadtree_pde_reconstructed_gradient(msh, du);
+[estimator] = bim2c_quadtree_pde_ZZ_estimator_du(msh, u);
+
+err(i) = norm(u - u_ex, inf);
+
+# Save solution to file.
+fclose all;
+filename = sprintf("sol_%d", i);
+if (exist([filename ".vtu"], "file"))
+    delete([filename ".vtu"]);
+endif
+fpl_vtk_write_field_quadmesh(filename, msh, {u, "u"; u_ex, "u_ex";
+                                             u - u_ex, "err";
+                                             du_x, "du_x"; du_y, "du_y";
+                                             du_x_ex, "du_x_ex"; du_y_ex, "du_y_ex";
+                                             du_x - du_x_ex, "err_du_x";
+                                             du_y - du_y_ex, "err_du_y"}, {estimator.', "estimator"}, 1);
+
+function [phi] = plot_phi5(msh, x, y)
+    phi5 = bim2c_quadtree_eval_fun(msh, 2, x, y)(:, 2) ...
+           + bim2c_quadtree_eval_fun(msh, 3, x, y)(:, 4) ...
+           + bim2c_quadtree_eval_fun(msh, 4, x, y)(:, 1) ...
+           + bim2c_quadtree_eval_fun(msh, 7, x, y)(:, 3);
+    
+    phi11 = bim2c_quadtree_eval_fun(msh, 6, x, y)(:, 3) ...
+            + bim2c_quadtree_eval_fun(msh, 7, x, y)(:, 2);
+    
+    phi12 = bim2c_quadtree_eval_fun(msh, 7, x, y)(:, 4) ...
+            + bim2c_quadtree_eval_fun(msh, 8, x, y)(:, 3);
+    
+    phi = phi5 + 0.5 * phi11 + 0.5 * phi12;
+endfunction
+
+x = linspace(0, 1, 100);
+y = x;
+
+[X, Y] = meshgrid(x, y);
+
+phi5 = reshape(plot_phi5(msh, X(:), Y(:)), size(X));
+#surf(X, Y, phi5);
