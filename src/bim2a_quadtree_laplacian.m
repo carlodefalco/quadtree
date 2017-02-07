70a00f10dcdd44eb7bc07e6584e96e8bb3029928
elauksap <pasquale.africa@libero.it>
2017-02-07 18:29:15 +0100

    Remove projection method

--- src/bim2a_quadtree_laplacian.m
+++ src/bim2a_quadtree_laplacian.m
@@ -18,42 +18,6 @@ function A = bim2a_quadtree_laplacian (msh, D)
   
   A = sparse (II, JJ, VV, numel (msh.reduced_to_full),
               numel (msh.reduced_to_full));
-  
-  
-    ## Projection method.
-#     persistent A_loc = [1 -0.5 0 -0.5;
-#                         -0.5 1 -0.5 0;
-#                         0 -0.5 1 -0.5;
-#                         -0.5 0 -0.5 1];
-#     
-#     Nnodes = columns(msh.p);
-# 
-#     for inode = 1 : 4
-#         for jnode = 1 : 4
-#             II(inode, jnode, :) = msh.t(inode, :);
-#             JJ(inode, jnode, :) = msh.t(jnode, :);
-#             VV(inode, jnode, :) = D .* A_loc(inode, jnode);
-#         endfor
-#     endfor
-#     
-#     A = sparse(II, JJ, VV, Nnodes, Nnodes);
-#     A = msh.P.' * A * msh.P;
-    
-    ## Constraints method.
-#     hanging = find(any(msh.hanging));
-#     non_hanging = find(!any(msh.hanging));
-#     
-#     II = repmat(1:numel(hanging), [2, 1])(:);
-#     JJ = msh.full_to_reduced(msh.hanging(:, hanging))(:);
-#     VV = -0.5 * ones(size(II));
-#     
-#     Ann = A(non_hanging, non_hanging);
-#     Anh = A(non_hanging, hanging);
-#     
-#     E = sparse(II, JJ, VV, numel(hanging), numel(non_hanging));
-#     D = speye(numel(hanging));
-#     
-#     A = Ann - Anh * (D \ E);
 endfunction
 
 function [II, JJ, VV] = local_to_global (msh, D, iel)
